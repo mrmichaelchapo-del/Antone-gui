fa98769d3c885ab91ebaf6814e2d2fdc1be4b059
mrmichaelchapo-del <mr.michaelchapo@gmail.com>
2026-09-12 17:11:44 -0400

    Create blockCalls.js

---
+++ Brickly/blockCalls.js
@@ -0,0 +1,104 @@
+export const motionBlocks = [
+    {
+        opcode: ["motion_movesteps"],
+        text: ("move _ steps")
+    },
+    {
+        opcode: ["motion_turnright"],
+        text: ("turn right _ degrees")
+    },
+    {
+        opcode: ["motion_turnleft"],
+        text: ("turn left _ degrees")
+    },
+    {
+        opcode: ["motion_goto"],
+        text: ("go to _")
+    },
+    {
+        opcode: ["motion_gotoxy"],
+        text: ("go to x: _ y: _")
+    },
+    {
+        opcode: ["motion_glideto"],
+        text: ("glide _ secs to _")
+    },
+    {
+        opcode: ["motion_glidesecstoxy"],
+        text: ("glide _ secs to x: _ y: _")
+    },
+    {
+        opcode: ["motion_pointindirection"],
+        text: ("point in direction _")
+    },
+    {
+        opcode: ["motion_pointtowards"],
+        text: ("point towards _")
+    },
+    {
+        opcode: ["motion_changexby"],
+        text: ("change x by _")
+    },
+    {
+        opcode: ["motion_setx"],
+        text: ("set x to _")
+    },
+    {
+        opcode: ["motion_changeyby"],
+        text: ("change y by _")
+    },
+    {
+        opcode: ["motion_sety"],
+        text: ("set y to _")
+    },
+    {
+        opcode: ["motion_ifonedgebounce"],
+        text: ("if on edge, bounce")
+    },
+    {
+        opcode: ["motion_setrotationstyle"],
+        text: ("set rotation style _")
+    },
+    {
+        opcode: ["motion_xposition"],
+        text: ("x position")
+    },
+    {
+        opcode: ["motion_yposition"],
+        text: ("y position")
+    },
+    {
+        opcode: ["motion_direction"],
+        text: ("direction")
+    },
+
+    // --- CUSTOM MOD EXCLUSIVE ---
+    {
+        opcode: ["motion_setvelocity"],
+        text: ("set velocity x: _ y: _")
+    },
+    {
+        opcode: ["motion_applyforce"],
+        text: ("apply force x: _ y: _")
+    },
+    {
+        opcode: ["motion_friction"],
+        text: ("apply friction _")
+    },
+    {
+        opcode: ["motion_followobject"],
+        text: ("follow object _ at speed _")
+    },
+    {
+        opcode: ["motion_orbitobject"],
+        text: ("orbit object _ radius _ speed _")
+    },
+    {
+        opcode: ["motion_snapto"],
+        text: ("snap to _")
+    },
+    {
+        opcode: ["motion_gotonigger"],
+        text: ("go to nigger _")
+    }
+];
