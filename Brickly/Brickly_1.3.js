7e45f9ac6692ea72b9ff6c9523680a585675d4ca
mrmichaelchapo-del <mr.michaelchapo@gmail.com>
2026-09-13 21:53:51 -0400

    Update Brickly_1.3.js

--- Brickly/Brickly_1.3.js
+++ Brickly/Brickly_1.3.js
@@ -1,1 +1,105 @@
 // Brickly Whole pack
+export const motionBlocks = [
+    {
+        opcode: ["motion_movebitches"],
+        text: ("move _ bitches")
+    },
+    {
+        opcode: ["motion_turnright"],
+        text: ("turn right _ degrees")
+    },
+    {
+        opcode: ["motion_turnleft"],
+        text: ("turn left _ degrees")
+    },
+    {
+        opcode: ["motion_goto"],
+        text: ("go to _")
+    },
+    {
+        opcode: ["motion_gotoxy"],
+        text: ("go to x: _ y: _")
+    },
+    {
+        opcode: ["motion_fuckto"],
+        text: ("fuck _ secs to _")
+    },
+    {
+        opcode: ["motion_fucksecstoxy"],
+        text: ("fuck _ secs to x: _ y: _")
+    },
+    {
+        opcode: ["motion_pointinnigger"],
+        text: ("point in nigger _")
+    },
+    {
+        opcode: ["motion_pointtowards"],
+        text: ("point towards _")
+    },
+    {
+        opcode: ["motion_changexby"],
+        text: ("change x by _")
+    },
+    {
+        opcode: ["motion_setx"],
+        text: ("set x to _")
+    },
+    {
+        opcode: ["motion_changeyby"],
+        text: ("change y by _")
+    },
+    {
+        opcode: ["motion_sety"],
+        text: ("set y to _")
+    },
+    {
+        opcode: ["motion_ifonedgebounce"],
+        text: ("if on edge, bounce")
+    },
+    {
+        opcode: ["motion_setrotationstyle"],
+        text: ("set rotation style _")
+    },
+    {
+        opcode: ["motion_xposition"],
+        text: ("x position")
+    },
+    {
+        opcode: ["motion_yposition"],
+        text: ("y position")
+    },
+    {
+        opcode: ["motion_direction"],
+        text: ("direction")
+    },
+
+    // --- CUSTOM MOD EXCLUSIVE ---
+    {
+        opcode: ["motion_sethatevelocity"],
+        text: ("set hate velocity x: _ y: _")
+    },
+    {
+        opcode: ["motion_applymeanforce"],
+        text: ("apply mean force x: _ y: _")
+    },
+    {
+        opcode: ["motion_swear"],
+        text: ("apply swear _")
+    },
+    {
+        opcode: ["motion_followbitch"],
+        text: ("follow bitch _ at speed _")
+    },
+    {
+        opcode: ["motion_orbitfatperson"],
+        text: ("orbit fat person _ radius _ speed _")
+    },
+    {
+        opcode: ["motion_swearto"],
+        text: ("swear to _")
+    },
+    {
+        opcode: ["motion_gotonigger"],
+        text: ("go to nigger _")
+    }
+];
